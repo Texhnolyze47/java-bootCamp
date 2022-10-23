fca4fe182a2303b9bbcfda2f129a23e1d558a20a
Texhnolyze <ichigobankai136@gmail.com>
2022-10-23 12:23:59 -0500

    se refactorizo el for en EjercicioSesion3

--- src/com/estructurascontrol/EjercicioSesion3.java
+++ src/com/estructurascontrol/EjercicioSesion3.java
@@ -2,9 +2,13 @@ package com.estructurascontrol;
 public class EjercicioSesion3 {
     public static void main(String[] args) {
         String[] nombres = {"Pepe", "Juan", "Anthony"};
+//
+//        for (int i = 0;i < nombres.length; i++ ){
+//            System.out.print(nombres[i] + " ");
+//        }
 
-        for (int i = 0;i < nombres.length; i++ ){
-            System.out.print(nombres[i] + " ");
+        for (String nombre: nombres) {
+            System.out.println(nombre + " ");
         }
     }
 }
