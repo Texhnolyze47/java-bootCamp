51293d66f59c808e72477c368b559c6bd60f0e0d
Texhnolyze <ichigobankai136@gmail.com>
2022-10-22 22:13:47 -0500

    add: Clase ConstrutorLargo

---
+++ src/com/javaavanzado/refactor/ConstrutorLargo.java
@@ -0,0 +1,17 @@
+package com.javaavanzado.refactor;
+
+public class ConstrutorLargo {
+
+    String marca;
+    String modelo;
+    String motor;
+    int puertas;
+
+    public ConstrutorLargo(String marca, String modelo, String motor, int puertas) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.motor = motor;
+        this.puertas = puertas;
+    }
+
+}
