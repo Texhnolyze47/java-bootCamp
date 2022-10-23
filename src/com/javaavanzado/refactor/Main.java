5cf9b9c190c2ccaf6fd1ca9e7b972b447fe70678
Texhnolyze <ichigobankai136@gmail.com>
2022-10-22 22:11:27 -0500

    add: metodo ifAnidado

--- src/com/javaavanzado/refactor/Main.java
+++ src/com/javaavanzado/refactor/Main.java
@@ -16,4 +16,15 @@ public class Main {
 
         System.out.println("Mayor: " + mayor + " Menor: " + menor);
     }
+
+    public static int ifAnidado1(){
+        int valor1 = 10;
+        int valor2 = 10;
+
+        if (valor1 > valor2){
+            return valor1;
+        }
+
+        return valor2;
+    }
 }
